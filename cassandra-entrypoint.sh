1ff83063e1901e8e569fa20b981f1dd40286b5e1
specnazm <laketic.milena98@gmail.com>
2022-04-10 14:20:03 +0200

    Figured properly kafka-connector

--- cassandra-entrypoint.sh
+++ cassandra-entrypoint.sh
@@ -2,11 +2,21 @@
 
 if [[ ! -z "$CASSANDRA_KEYSPACE" && $1 = 'cassandra' ]]; then
   # Create default keyspace for single node cluster
-  CQL="CREATE KEYSPACE $CASSANDRA_KEYSPACE WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1};"
-  until echo $CQL| cqlsh; do
+  CQL="CREATE KEYSPACE IF NOT EXISTS $CASSANDRA_KEYSPACE WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1};"
+  until echo $CQL | cqlsh; do
     echo "cqlsh: Cassandra is unavailable - retry later"
     sleep 2
   done &
 fi
 
-exec /cassandra-entrypoint.sh "$@"
+if [[ ! -z "$CASSANDRA_KEYSPACE" && "$CASSANDRA_TABLE" && $1 = 'cassandra' ]]; then
+  # Create default keyspace for single node cluster
+  CQL_TABLE="CREATE TABLE IF NOT EXISTS $CASSANDRA_KEYSPACE.$CASSANDRA_TABLE (game_id text, fen text, lm text, wc int, bc int, PRIMARY KEY(game_id, wc));"
+  until echo $CQL_TABLE | cqlsh; do
+    echo "cqlsh: Cassandra is unavailable - retry later table"
+    sleep 2
+  done &
+fi
+
+
+exec /docker-entrypoint.sh "$@"
--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,32 +1,45 @@
 import os
 import faust
 from faust.models.fields import Optional
+from stockfish import Stockfish
 
 class GameEvent(faust.Record):
     id: int
     name: str
 
 
-class GameMove(faust.Record):
+class GameMove(faust.Record, serializer='json'):
+    game_id: Optional[int]
     fen: str
     lm: Optional[str]
     wc: int
     bc: int
 
 
-
-
-app = faust.App('consumer', broker='kafka://' + os.environ['KAFKA'], key_serializer='json')
+app = faust.App('consumer', broker=[
+            "kafka://" + os.environ['KAFKA1'],
+            "kafka://" + os.environ['KAFKA2'],
+            "kafka://" + os.environ['KAFKA3'],
+        ], key_serializer='json')
 moves_topic = app.topic('game-moves', value_type=GameMove)
 events_topic = app.topic('game-events', value_type=bytes)
+cassandra_sink = app.topic('cassandra-sink', value_type=bytes)
+
+stockfish = Stockfish(path="./stockfish_14.1_linux_x64")
+
+GAME_ID = None
 
 @app.agent(moves_topic)
 async def move_played(moves):
     async for move in moves:
+        move.game_id = GAME_ID
         print(f'New move:  {move}')
+        await cassandra_sink.send(key=GAME_ID, value=move.dumps())
 
 
 @app.agent(events_topic)
 async def game_event(events):
+    global GAME_ID
     async for event in events:
         print(f'New EVENT:  {event}')
+        GAME_ID = event["id"]
